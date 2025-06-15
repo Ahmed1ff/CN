30d2417e96a2d2c7b24944cc55e098054c9d2d21
mo7amednabih <eid46060@gmail.com>
2025-06-15 17:02:01 +0300

    update

--- app.py
+++ app.py
@@ -9,21 +9,26 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
 app = Flask(__name__)
 
-model = keras.models.load_model('./baby_cry_model_best_fan.h5')
-encoder = joblib.load('./label_encoder.pkl')
+# تحميل الموديل الجديد والـ encoder والـ scaler
+model = keras.models.load_model('./baby_cry_model_best_new20.keras')
+encoder = joblib.load('./label_encoder_new20.pkl')
+scaler = joblib.load('./scaler_new20.pkl')
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+# استخراج الخصائص من ملف الصوت وتطبيق الـ scaler
 def extract_features(file_path):
-    data, _ = librosa.load(file_path, sr=None, res_type='kaiser_fast')
-    mfccs = librosa.feature.mfcc(y=data, sr=22050, n_mfcc=40)
-    return np.mean(mfccs.T, axis=0)
+    data, _ = librosa.load(file_path, sr=16000, res_type='kaiser_fast')  # تأكد من sr = 16000 زي التدريب
+    mfccs = librosa.feature.mfcc(y=data, sr=16000, n_mfcc=40)
+    feature = np.mean(mfccs.T, axis=0)
+    feature_scaled = scaler.transform([feature])
+    return feature_scaled
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
-    
+
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
@@ -32,16 +37,15 @@ def predict():
     file.save(file_path)
 
     try:
-        features = extract_features(file_path)
-        features = features.reshape(1, 40, 1)
+        features = extract_features(file_path)  # (1, 40)
+        features = np.expand_dims(features, axis=2)  # (1, 40, 1)
 
         predictions = model.predict(features)
-        probabilities = predictions.flatten()  
+        probabilities = predictions.flatten()
 
         categories = encoder.classes_
 
         class_probabilities = {category: f"{prob*100:.2f}%" for category, prob in zip(categories, probabilities)}
-
         predicted_label = categories[np.argmax(probabilities)]
 
         os.remove(file_path)
